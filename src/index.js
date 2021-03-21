e8e109c89bb40c1cc9209607352058b0c530ca6f
Raphael <corujoraphael@gmail.com>
2021-03-21 20:16:07 -0300

    Sequelize and docker adjustments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 'use strict'
-
-const express = require('express')
-const swaggerUi = require('swagger-ui-express')
+require('dotenv').config()
+const express     = require('express')
+const swaggerUi   = require('swagger-ui-express')
 const swaggerFile = require('../swagger_output.json')
+const bodyParser  = require('body-parser')
+const cors        = require('cors')
 const app = express()
 
-const PORT = 8080
+app.use(bodyParser.json({limit : "50mb"})); // support json encoded bodies
+app.use(bodyParser.urlencoded({ limit : "50mb", extended: true })); // support encoded bodies
+app.use(cors({
+     origin: "*",
+     methods: ['GET', 'PUT', 'POST', 'DELETE'],
+     allowedHeaders : ['Origin', 'X-Requested-With', 'content-type', 'Authorization', 'Token', 'Folder']
+}));
 
+const PORT = 8080
 
 function onStart(){
     console.log(`Server running on port ${PORT}`)
@@ -15,7 +24,7 @@ function onStart(){
 app.listen(PORT, onStart)
 
 
-app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerFile, { explorer: true }))
+app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 const apiRouter = require('./routes/api')
 app.use('/api', apiRouter)
